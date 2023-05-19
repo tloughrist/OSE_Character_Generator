97433dcfab457fa5215d3b3e6245b712432ecf8b
timloughrist <tim.loughrist@gmail.com>
2023-05-19 05:26:47 -0500

    set default font to courier new

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        mono: ['Courier New', ...defaultTheme.fontFamily.mono]
       },
     },
   },
